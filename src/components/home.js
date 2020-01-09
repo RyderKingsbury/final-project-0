df74c20b296191e7d9493d76d4e6ab6363e52ee5
RyderKingsbury <kingsbury.ryder@gmail.com>
2020-01-08 21:01:51 -0700

    final

---
+++ src/components/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class home extends Component {
+    render() {
+        return (
+            <div>
+                Welcome to thy homepage
+            </div>
+        );
+    }
+}
+
+export default home;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './ini/index.css';
+import App from './ini/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
